154bdada2ce268e59b061f10543c21675ced2a5d
pavel-surzhenko <pavel.surzhenko@icloud.com>
2025-04-01 17:05:18 +0300

    clean files

--- src/app/layouts/app.layout.tsx
+++ src/app/layouts/app.layout.tsx
@@ -1,5 +1,5 @@
 const AppLayout = ({ children }) => {
-  return <div className="min-h-screen mx-auto">{children}</div>;
+  return <div className="min-h-screen mx-auto w-full">{children}</div>;
 };
 
 export default AppLayout;
