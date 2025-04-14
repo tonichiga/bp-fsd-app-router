962ace8be74c20ab165b315dd618eac65ba6656a
pavel-surzhenko <pavel.surzhenko@icloud.com>
2025-04-14 14:27:51 +0300

    fix

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/07.shared/components/ui/button";
 import { ThemeChanger } from "@/07.shared/ui";
 
 const Home = () => {
@@ -7,7 +6,6 @@ const Home = () => {
       <div className="text-mint-500  tablet:text-red-500 w-full grid place-items-center  font-bold font-Fira">
         HELLO WORLD
       </div>
-      <Button>123123</Button>
 
       <ThemeChanger />
     </div>
